5894959340c0163edbdcd9221e9aa71ff2ee197e
Chandrut22 <chandrutd22@gmail.com>
2025-04-29 13:13:04 +0530

    Update the Allowed host

--- diseases_detector/settings.py
+++ diseases_detector/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8g_h)a%!8j8m$2+p$-2f3po*=hs29_e!a)_svve%4&(_-0xzma'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["leafpredict-plant-disease-detection.onrender.com","https://leafpredict-plant-disease-detection.onrender.com","http://127.0.0.1:8000/"]
 
 
 # Application definition
